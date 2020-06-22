e17072e7f9383fa1aae5ae4fdccc6d951aa29404
JokerV <Quocvietcoi2000@gmail.com>
2020-06-22 22:48:20 +0700

    Pokemons Cotainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
 
 function App () {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh/'
+  });
+
   return (
-    <p>Hello</p>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
   );
 }
 
